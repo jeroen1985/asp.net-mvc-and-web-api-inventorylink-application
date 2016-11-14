6141d76ea9b4df8c6a8caf05ca7c170931f0c92d
Jeroen Stevens <jeroenstevens@gmail.com>
2016-11-13 19:36:41 -0800

    Add initial API controllers and helper (Extension Method) for versioning

---
+++ InventoryLink2/Helpers/Version.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace InventoryLink2.Helpers
+{
+    public static class Version
+    {
+        public static string defaultVersion = "v1";
+
+        public static string getVersion(this HttpRequestMessage request)
+        {
+            if (request.Headers.Contains("version"))
+            {
+                string version = request.Headers.GetValues("version").FirstOrDefault();
+
+                if (!string.IsNullOrEmpty(version))
+                {
+                    return version;
+                }
+
+            }
+
+            return defaultVersion;
+
+        }
+
+    }
+}
